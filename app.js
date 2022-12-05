25df76f1bb20228345a4c3646e3430915a693f2b
SPehsar <cloud_data_sec_mgmt@yahoo.com>
2022-12-05 07:33:31 -0500

    first commit2nd

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const Dashboard = () => {
         <h1>Dashboard</h1>
         <h2>Widget</h2>
         <h2>Reviews</h2>
+        <h2>Customers</h2>
         <h2>Online Analysis</h2>
         <h2>Settings</h2>
       </div>
